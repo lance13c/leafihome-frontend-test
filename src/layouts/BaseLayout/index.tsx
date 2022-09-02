544245516318b96f934d1b680da8494cc2adf828
Dominic Cicilio <dominic@yoamigo.io>
2022-09-02 13:05:40 -0400

    adds children to BaseLayout

--- src/layouts/BaseLayout/index.tsx
+++ src/layouts/BaseLayout/index.tsx
@@ -3,8 +3,8 @@ import styles from './index.module.scss';
 
 interface BaseLayoutProps {}
 
-const BaseLayout: React.FunctionComponent<BaseLayoutProps> = () => {
-  return <div className={styles.BaseLayout}></div>;
+const BaseLayout: React.FunctionComponent<BaseLayoutProps> = ({ children }) => {
+  return <div className={styles.BaseLayout}>{children}</div>;
 };
 
 export default BaseLayout;
