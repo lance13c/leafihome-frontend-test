835ef284705066a5bc61d3888cc576468dd006b5
Dominic Cicilio <dominic@yoamigo.io>
2022-09-02 13:04:48 -0400

    adds scss with module fix for parcel, organizes files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import CompaniesPage from './pages/CompaniesPage';
+import HomePage from './pages/HomePage';
 
 interface AppProps {}
 
 const App: React.FunctionComponent<AppProps> = () => {
-  return <h1>Hello</h1>;
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/companies' element={<CompaniesPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
