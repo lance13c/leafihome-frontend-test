979b05dc9d7389433e238162696c3e3fc30a5ea8
Dominic Cicilio <dominic@yoamigo.io>
2022-09-07 18:08:46 -0400

    adds some type to companies page

--- client/src/components/CompanyCard/index.tsx
+++ client/src/components/CompanyCard/index.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Company } from '../../../../server/types';
 import styles from './index.module.scss';
 
 interface CompanyCardProps extends Company {}
 
 const CompanyCard: React.FunctionComponent<CompanyCardProps> = ({ name, address, revenue, phone, _id: id }) => {
-  return (
-    <div className={styles.companyCard}>
-      <div className={styles.companyCardHeader}>
-        <h3>{name}</h3>
-      </div>
+  const navigate = useNavigate();
 
-      <p>{id}</p>
-      <p>{address}</p>
-      <p>{phone}</p>
-      <h6>${revenue}</h6>
+  const handleOnClick = () => {
+    navigate(`/companies/${id}`);
+  };
+
+  return (
+    <div className={styles.companyCard} onClick={handleOnClick}>
+      <div className={styles.companyCardHeader}>
+        <h2>{name}</h2>
+      </div>
     </div>
   );
 };
